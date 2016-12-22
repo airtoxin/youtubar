c6158b8a1535bee6e620e909086de79a8de4745e
airtoxin <airtoxin@icloud.com>
2016-12-22 17:01:25 +0900

    Use menubar

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
-import { app, BrowserWindow } from 'electron';
+import menubar from 'menubar';
 import main from './browser';
 
-let mainWindow;
+const mb = menubar();
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 200,
-    height: 200,
-    webPreferences: {
-      webSecurity: false,
-    },
-    // resizable: false,
-    show: true,
-    // transparent: true,
-    // frame: false,
-    // alwaysOnTop: true,
-  });
-
-  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
-
-  mainWindow.webContents.openDevTools({ detach: true });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-main(mainWindow);
+main(mb.window);
