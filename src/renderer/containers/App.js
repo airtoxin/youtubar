3371763f8b968d6065b1e6162200efbbad6bcb2b
airtoxin <airtoxin@icloud.com>
2016-12-23 16:53:33 +0900

    Let's use webpack

--- src/renderer/containers/App.js
+++ src/renderer/containers/App.js
@@ -5,9 +5,16 @@ import { branch } from 'baobab-react/higher-order';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello, youtubar!</h1>
-        <p>count: {this.props.count}</p>
+      <div className="window">
+        <header className="toolbar toolbar-header">
+          <h1 className="title">Header</h1>
+        </header>
+        <div className="window-content">
+          <div className="pane-group">
+            <div className="pane-sm sidebar">...</div>
+            <div className="pane">...</div>
+          </div>
+        </div>
       </div>
     );
   }
