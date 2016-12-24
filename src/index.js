3d9971b209e556d687e45461c900e56d2158c0ac
airtoxin <airtoxin@icloud.com>
2016-12-24 21:36:57 +0900

    Use babel plugin for css-module

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import menubar from 'menubar';
 
 const mb = menubar({
   index: `file://${process.cwd()}/dest/renderer/index.html`,
+  alwaysOnTop:true,
+});
+
+mb.on('after-create-window', () => {
+  mb.window.openDevTools();
 });
--- src/renderer/containers/App.js
+++ src/renderer/containers/App.js
@@ -1,18 +1,20 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component, PropTypes } from 'react';
 import { branch } from 'baobab-react/higher-order';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="window">
         <header className="toolbar toolbar-header">
-          <h1 className="title">Header</h1>
+          <div className={`title ${styles.flex}`}>
+            <div>Header</div>
+            <div className={styles.right}>Clear</div>
+          </div>
         </header>
         <div className="window-content">
           <div className="pane-group">
-            <div className="pane-sm sidebar">...</div>
-            <div className="pane">...</div>
           </div>
         </div>
       </div>
