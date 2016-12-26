46576b81a45a9c05b51943a98e013cc723813cfd
airtoxin <airtoxin@icloud.com>
2016-12-26 17:49:43 +0900

    Add stylings

--- src/renderer/components/SearchItem.js
+++ src/renderer/components/SearchItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './SearchItem.css';
 
 export default ({ item }) => {
   console.log("@item", item)
   return (
-    <div>
-      {item.id.videoId}
+    <div className={styles.flex}>
+      <img className={styles.thumbnail} src={item.snippet.thumbnails.default.url}/>
+      <div>
+        <div>{item.snippet.title}</div>
+        <div>{item.snippet.description}</div>
+      </div>
     </div>
   );
 };
