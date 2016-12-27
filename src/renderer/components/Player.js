d15b86970fcad772d3386e4911f593fb4dd4b0a5
airtoxin <airtoxin@icloud.com>
2016-12-27 20:23:58 +0900

    PlayPause-able

--- src/renderer/components/Player.js
+++ src/renderer/components/Player.js
@@ -1,22 +1,50 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { branch } from 'baobab-react/higher-order';
 import Youtube from 'react-youtube';
 import styles from './Player.css';
 
-const Player = ({ playing }) => {
-  if (!playing) return null;
+class Player extends Component {
+  constructor() {
+    super();
 
-  return (
-    <div className={styles.hidden}>
-      <Youtube
-        videoId={playing.id.videoId}
-        id="playing-video"
-        onReady={event => event.target.playVideo()}
-      />
-    </div>
-  );
+    this.api = null;
+
+    this.handleOnReady = this.handleOnReady.bind(this);
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!nextProps.playing) return;
+    if (!this.api) return;
+
+    switch (nextProps.player) {
+      case 'playing':
+        return this.api.playVideo();
+      case 'paused':
+        return this.api.pauseVideo();
+    }
+  }
+
+  handleOnReady(event) {
+    event.target.playVideo();
+    this.api = event.target;
+  }
+
+  render() {
+    if (!this.props.playing) return null;
+
+    return (
+      <div className={styles.hidden}>
+        <Youtube
+          videoId={this.props.playing.id.videoId}
+          id="playing-video"
+          onReady={this.handleOnReady}
+        />
+      </div>
+    );
+  }
 }
 
 export default branch({
   playing: ['playing'],
+  player: ['player'],
 }, Player);
